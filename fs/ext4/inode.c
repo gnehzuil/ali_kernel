2e7a381c45f2c50d11d17c670f0e44f4a96664ae
Theodore Ts'o <tytso@mit.edu>
2013-08-19 16:15:17 +0800

    ext4: fix ext4_end_io_dio() racing against fsync()
    We need to make sure iocb->private is cleared *before* we put the
    io_end structure on i_completed_io_list.  Otherwise fsync() could
    potentially run on another CPU and free the iocb structure out from
    under us.
    
    [Note from Zheng Liu]
    This patch should be dropped after we rebase against the newer
    kernel with dioread_nolock feature.
    
    Patch-mainline: v3.2-rc5-1-gb5a7e97
    Reference: b5a7e97039a80fae673ccc115ce595d5b88fb4ee
    Reported-by: Kent Overstreet <koverstreet@google.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Cc: stable@kernel.org
    Signed-off-by: Zheng Liu <wenqing.lz@taobao.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -4150,10 +4150,11 @@ static void ext4_end_io_dio(struct kiocb *iocb, loff_t offset,
  		  iocb->private, io_end->inode->i_ino, iocb, offset,
 		  size);
 
+	iocb->private = NULL;
+
 	/* if not aio dio with unwritten extents, just free io and return */
 	if (io_end->flag != DIO_AIO_UNWRITTEN){
 		ext4_free_io_end(io_end);
-		iocb->private = NULL;
 out:
 		if (is_async)
 			aio_complete(iocb, ret, 0);
@@ -4175,7 +4176,6 @@ out:
 	spin_unlock_irqrestore(&ei->i_completed_io_lock, flags);
 
 	/* queue the work to convert unwritten extents to written */
-	iocb->private = NULL;
 	queue_work(wq, &io_end->work);
 }
 /*
